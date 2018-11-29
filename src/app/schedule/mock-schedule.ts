5930f90dba2fc03d2ecbdd753951151cc64c6e90
sylvia villegas <sylvia@svillegas.com>
2018-11-29 11:21:18 -0500

    Extract mock schedule to own file

---
+++ src/app/schedule/mock-schedule.ts
@@ -0,0 +1,40 @@
+import { Schedule } from "./models/schedule.model";
+
+export const SCHEDULE: Schedule = {
+  start: new Date(2018, 11, 10),
+  end: new Date(2018, 11, 12),
+  resources: [
+    {
+      name: 'John',
+      events: [
+        {
+          start: new Date(2018, 11, 11, 11),
+          end: new Date(2018, 11, 11, 12),
+          name: 'work order #200',
+          url: '#'
+        },
+        {
+          start: new Date(2018, 11, 12, 9),
+          end: new Date(2018, 11, 12, 12),
+          name: 'work order #222',
+          url: '#'
+        }
+      ]
+    },
+    {
+      name: 'Truck278',
+      events: [
+        {
+          start: new Date(2018, 11, 11, 11),
+          end: new Date(2018, 11, 11, 12),
+          name: 'work order #200',
+          url: '#'
+        }
+      ]
+    },
+    {
+      name: 'Truck452',
+      events: []
+    }
+  ]
+}
--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Event } from "./models/event.model";
 import { Resource } from "./models/resource.model";
 import { Schedule } from "./models/schedule.model";
+import { SCHEDULE } from "./mock-schedule";
 
 @Component({
   selector: 'app-schedule',
@@ -12,44 +13,7 @@ import { Schedule } from "./models/schedule.model";
 
 export class ScheduleComponent implements OnInit {
 
-  schedule: Schedule = {
-    start: new Date(2018, 11, 10),
-    end: new Date(2018, 11, 12),
-    resources: [
-      {
-        name: 'John',
-        events: [
-          {
-            start: new Date(2018, 11, 11, 11),
-            end: new Date(2018, 11, 11, 12),
-            name: 'work order #200',
-            url: '#'
-          },
-          {
-            start: new Date(2018, 11, 12, 9),
-            end: new Date(2018, 11, 12, 12),
-            name: 'work order #222',
-            url: '#'
-          }
-        ]
-      },
-      {
-        name: 'Truck278',
-        events: [
-          {
-            start: new Date(2018, 11, 11, 11),
-            end: new Date(2018, 11, 11, 12),
-            name: 'work order #200',
-            url: '#'
-          }
-        ]
-      },
-      {
-        name: 'Truck452',
-        events: []
-      }
-    ]
-  }
+  schedule: Schedule = SCHEDULE;
 
   constructor() { }
 
