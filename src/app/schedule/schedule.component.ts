036a47dd5cfd2c20e3097478b5a141f770170131
sylvia villegas <sylvia@svillegas.com>
2018-11-29 10:02:44 -0500

    Add basic schedule variables as mock schedule

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Event } from "./models/event.model";
+import { Schedule } from "./models/schedule.model";
+
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
   styleUrls: ['./schedule.component.css']
 })
+
 export class ScheduleComponent implements OnInit {
+
+  schedule: Schedule = {
+    start: new Date(2018, 11, 10),
+    end: new Date(2018, 11, 12),
+    resources: ['John', 'truck 1', 'truck 22' ],
+    events: [
+      {
+        start: new Date(2018, 11, 11, 11),
+        end: new Date(2018, 11, 11, 12),
+        name: 'work',
+        url: '#'
+      }
+    ]
+  }
 
   constructor() { }
 
